77b30ea1b2b0acfa70a0c7737919eac19d2876f7
Sudip Denre <sudipdenre4@gmail.com>
2023-07-29 12:02:31 +0530

    second commit

--- Level1.java
+++ Level1.java
@@ -1,5 +1,6 @@
 public class Level1{
     public static void main(String[] args) {
         System.out.println("You are in Level 1");
+        System.out.println("Now You are in Level 2");
     }
 } 
